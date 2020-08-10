e5d781dc3b5913943e62b66125b5a8ef11b4aebb
Ishika <ishukiki2002@gmail.com>
2020-08-10 23:53:41 +0530

    Upload Images

---
+++ public/mapdata.js
@@ -0,0 +1,159 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+   //General settings
+    width: "700", //'700' or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+
+    //State defaults
+    state_description: "State description",
+    state_color: "orange",
+    state_hover_color: "darkorange",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+
+    //Location defaults
+    location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+
+    //Label defaults
+    label_color: "#d5ddec",
+    label_hover_color: "#d5ddec",
+    label_size: 22,
+    label_font: "Arial",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+
+    //Zoom settings
+    zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+
+    //Popup settings
+    popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+
+    //Advanced settings
+    div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website",
+    popups: "detect",
+    state_image_url: "",
+    state_image_position: "",
+    location_image_url: ""
+  },
+  state_specific: {
+    BTN2433: {
+      name: "Chhukha",
+      inactive: "yes"
+    },
+    BTN2434: {
+      name: "Daga",
+      inactive: "yes"
+    },
+    BTN2435: {
+      name: "Ha",
+      inactive: "yes"
+    },
+    BTN2436: {
+      name: "Paro",
+      inactive: "yes"
+    },
+    BTN2437: {
+      name: "Gasa",
+      inactive: "yes"
+    },
+    BTN2438: {
+      name: "Samchi",
+      inactive: "yes"
+    },
+    BTN2439: {
+      name: "Thimphu",
+      inactive: "yes"
+    },
+    BTN2440: {
+      name: "Punakha",
+      inactive: "yes"
+    },
+    BTN2480: {
+      name: "Bumthang",
+      inactive: "yes"
+    },
+    BTN2481: {
+      name: "Chirang",
+      inactive: "yes"
+    },
+    BTN2482: {
+      name: "Geylegphug",
+      inactive: "yes"
+    },
+    BTN2483: {
+      name: "Lhuntshi"
+    },
+    BTN2484: {
+      name: "Tashi Yangtse",
+      inactive: "yes"
+    },
+    BTN2485: {
+      name: "Shemgang",
+      inactive: "yes"
+    },
+    BTN2486: {
+      name: "Tongsa"
+    },
+    BTN2487: {
+      name: "Wangdi Phodrang"
+    },
+    BTN2490: {
+      name: "Mongar",
+      inactive: "yes"
+    },
+    BTN2491: {
+      name: "Pemagatsel"
+    },
+    BTN2492: {
+      name: "Samdrup Jongkhar"
+    },
+    BTN2493: {
+      name: "Tashigang"
+    }
+  },
+  locations: {
+    "0": {
+      lat: "27.473932",
+      lng: "89.635305",
+      name: "Thimphu"
+    }
+  },
+  labels: {},
+  regions: {}
+};
