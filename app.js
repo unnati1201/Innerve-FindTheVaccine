ea2cf75ac65777c8c0b40c3939ce7043d0dd605c
Unnati <unnat4ucreative@gmail.com>
2020-08-15 16:49:51 +0530

    Backend Working from home to map page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mapNames = require("./mapNames.js");
 
 const app = express();
 
@@ -8,12 +9,33 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
+let names = [];
+let mapImg = [];
+let mapUrl = [];
+
 app.get("/", (req,res)=>{
-  res.render("home");
+  mapNames.mapNames().forEach(function(item){
+    const newItem = item.charAt(0).toUpperCase() + item.slice(1);
+    names.push(newItem);
+    let newImg = "/images/"+newItem + ".png";
+    mapImg.push(newImg);
+    let newUrl = "/country/"+item;
+    mapUrl.push(newUrl);
+  })
+  res.render("home", {mapNames: names, mapImg: mapImg, mapUrl: mapUrl});
 });
 
-app.get("/country", (req,res)=>{
-  res.render("country");
+app.get("/country/:name", (req,res)=>{
+  const name = req.params.name;
+  mapNames.mapNames().forEach(function(item){
+    if(item === name){
+      const newItem = item.charAt(0).toUpperCase() + item.slice(1);
+      const mapData = "/" + name + "/mapData" + name + ".js";
+      const countryMap = "/" + name + "/countrymap" + name + ".js";
+      res.render("country", {mapName: newItem, smallName: name, mapData: mapData, countryMap: countryMap});
+      res.end();
+    }
+  });
 });
 
 app.listen(3000, (req,res)=>{
---
+++ mapNames.js
@@ -0,0 +1,26 @@
+module.exports = {
+  mapNames: function(){
+    return [
+      "albania",
+      "andorra",
+      "armenia",
+      "austria",
+      "bangladesh",
+      "barbados",
+      "belarus",
+      "belgium",
+      "belize",
+      "benin",
+      "bolivia",
+      "botswana",
+      "cameroon",
+      "costa rica",
+      "cyprus",
+      "djibouti",
+      "dominica",
+      "eritrea",
+      "ethiopia",
+      "gabon"
+    ]
+  }
+}
